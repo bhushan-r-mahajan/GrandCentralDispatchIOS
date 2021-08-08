5b3ae1bd82410cbe1727c95728cafb8a7eed9f77
Bhushan Mahajan <bhushanmahajan1997@gmail.com>
2021-08-08 15:19:13 +0530

    Implemented grandCEntraldispatch demo

---
+++ GCD/CollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  CollectionViewCell.swift
+//  GCD
+//
+//  Created by Gadgetzone on 08/08/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.clipsToBounds = true
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    weak var image: UIImage? {
+        didSet {
+            self.imageView.image = image
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        imageView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- GCD/ViewController.swift
+++ GCD/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    //MARK: - Properties
+    
+    var images = [UIImage]()
+    let textlabel = UILabel()
+    let imageURL = "https://c4.wallpaperflare.com/wallpaper/245/816/876/one-piece-monkey-d-luffy-trafalgar-law-ussop-wallpaper-preview.jpg"
+    
+    //MARK: - Init
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        loadImages()
     }
 
+    //MARK: - Configuration Functions
 
+    func loadImages() {
+        for _ in 1...50 {
+            downloadImages()
+        }
+    }
+    
+    private func downloadImages() {
+        DispatchQueue.global(qos: .utility).async { [weak self] in
+            URLSession.shared.dataTask(with: URL(string: self!.imageURL)!) { [weak self] data, response, error in
+                guard let self = self, let data = data, let image = UIImage(data: data) else { return }
+                self.images.append(image)
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            }.resume()
+        }
+    }
+    
+    //MARK: - CollectionView Delegate and Datasource Functions
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.layer.shouldRasterize = true
+        cell.layer.rasterizationScale = UIScreen.main.scale
+        cell.image = images[indexPath.row]
+        cell.backgroundColor = .clear
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width / 2, height: collectionView.frame.size.height / 4)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
 }
+
+
 
